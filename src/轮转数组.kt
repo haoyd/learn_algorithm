82a94cb4ab77e5f0ef7874c28a1fc7a5b17b3c9e
hyd <eric_hyd@foxmail.com>
2022-07-05 10:25:39 +0800

    feat：轮转数组

---
+++ src/轮转数组.kt
@@ -0,0 +1,34 @@
+/**
+    输入: nums = [1,2,3,4,5,6,7], k = 3
+    输出: [5,6,7,1,2,3,4]
+    解释:
+    向右轮转 1 步: [7,1,2,3,4,5,6]
+    向右轮转 2 步: [6,7,1,2,3,4,5]
+    向右轮转 3 步: [5,6,7,1,2,3,4]
+ */
+object 轮转数组 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val intArray = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+        rotate(intArray, 3)
+
+        for (i in intArray) {
+            print("$i ")
+        }
+    }
+
+    private fun rotate(nums: IntArray, k: Int) {
+        val length = nums.size
+
+        val newArr = IntArray(length)
+        for (i in nums.indices) {
+            val pos = (i + k) % length
+            newArr[pos] = nums[i]
+        }
+
+        for (i in nums.indices) {
+            nums[i] = newArr[i]
+        }
+    }
+}
