e6dfc75362dcc8f841660d466ca361bc0ceea787
hyd <eric_hyd@foxmail.com>
2022-07-07 21:53:04 +0800

    feat：二分查找换序位置

---
+++ src/二分查找换序位置.kt
@@ -0,0 +1,42 @@
+
+object 二分查找换序位置 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val intArray = intArrayOf(3, 4, 5, 1, 2)
+        val target = 1
+        val index = findTarget(intArray, target)
+        println("您找的是第 $index 个元素")
+    }
+
+    private fun findTarget(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+
+        val startValue = nums[0]
+        val endValue = nums[end]
+        val resorted = startValue > endValue
+
+        while (start <= end) {
+            val middle = (end - start) / 2 + start
+            val middleValue = nums[middle]
+
+            if (target == middleValue) {
+                return middle
+            }
+
+            if (target < middleValue) {
+                if (target < startValue) {
+                    start = middle + 1
+                } else {
+                    end = middle - 1
+                }
+            } else {
+                start = middle + 1
+            }
+
+        }
+
+        return -1
+    }
+}
